f771a8b350e16a8af9d0597010f03cec47e8d917
Johannes Wilm <mail@johanneswilm.org>
2026-06-24 18:55:22 +0200

    add all the functions

---
+++ src/file/index.js
@@ -0,0 +1,9 @@
+export {FileDialog} from "./dialog.js"
+export {FileSelector} from "./selector.js"
+export {
+    cleanPath,
+    moveFile,
+    shortFileTitle,
+    longFilePath
+} from "./tools.js"
+export {NewFolderDialog} from "./new_folder_dialog.js"

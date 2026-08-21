0699c57ea4008de6e589a6f1b3011f46eeeae77f
Johannes Wilm <mail@johanneswilm.org>
2026-08-21 17:35:00 +0200

    Replace Codeberg URLs with git.fiduswriter.org

--- scripts/deploy-pages.sh
+++ scripts/deploy-pages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Build the demo site and push it to the Codeberg Pages branch.
+# Build the demo site and push it to the Forgejo Pages branch.
 set -e
 
 ROOT=$(cd "$(dirname "$0")/.." && pwd)
@@ -26,7 +26,7 @@ cd "$BUILD_DIR"
 git init
 git checkout -b pages
 git add .
-git commit -m "Deploy fwtoolkit demo to Codeberg Pages"
+git commit -m "Deploy fwtoolkit demo to Forgejo Pages"
 
 REMOTE=$(cd "$ROOT" && git remote get-url origin)
 echo "Pushing to $REMOTE pages branch..."
@@ -35,4 +35,4 @@ git push -f origin pages
 
 cd "$ROOT"
 rm -rf "$BUILD_DIR"
-echo "Done. The demo should be available at https://fiduswriter.codeberg.page/fwtoolkit/"
+echo "Done. The demo should be available at https://fiduswriter.pages.fiduswriter.org/fwtoolkit/"
